3f99869fd0579359d401f14f98d61bc2324cc289
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2018-08-17 02:41:52 -0500

    add optimized Keccak implementation
    credit Joseph Robert Melsha for original;
    20% more throughput than bouncycastle Keccak.Digest256 (average input length 33 bytes);
    supports variable-length digest

---
+++ src/main/java/com/joemelsha/crypto/hash/Keccak.java
@@ -0,0 +1,422 @@
+package com.joemelsha.crypto.hash;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.security.MessageDigest;
+
+/**
+ * @author Joseph Robert Melsha (jrmelsha@olivet.edu)
+ *
+ * Source: https://github.com/jrmelsha/keccak
+ * Created: Jun 23, 2016
+ *
+ * Copyright 2016 Joseph Robert Melsha
+ * Changed by Evan Saulpaugh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class Keccak extends MessageDigest {
+
+    private static final int MAX_STATE_SIZE = 1600;
+    private static final int MAX_STATE_SIZE_WORDS = MAX_STATE_SIZE / 64;
+
+    protected final int digestSizeBits;
+    protected final transient int digestSizeBytes;
+    protected final transient int rateSizeBits;
+    private final transient int rateSizeWords;
+
+    private final long[] state = new long[MAX_STATE_SIZE_WORDS];
+    private int rateBits = 0;
+
+    protected transient ByteBuffer out;
+
+    public Keccak(int digestSizeBytes) {
+        this("Keccak-", digestSizeBytes);
+    }
+
+    protected Keccak(String variantPrefix, int digestSizeBits) {
+        super(variantPrefix + digestSizeBits);
+        int rateSizeBits = rateSizeBitsFor(digestSizeBits);
+        if (rateSizeBits + digestSizeBits * 2 != MAX_STATE_SIZE)
+            throw new IllegalArgumentException("Invalid rateSizeBits + digestSizeBits * 2: " + rateSizeBits + " + " + digestSizeBits + " * 2 != " + MAX_STATE_SIZE);
+        if (rateSizeBits <= 0 || (rateSizeBits & 0x3f) > 0)
+            throw new IllegalArgumentException("Invalid rateSizeBits: " + rateSizeBits);
+
+        this.digestSizeBits = digestSizeBits;
+        this.digestSizeBytes = digestSizeBits >>> 3;
+
+        this.rateSizeBits = rateSizeBits;
+        this.rateSizeWords = rateSizeBits >>> 6;
+    }
+
+    public Keccak(Keccak other) {
+        super(other.getAlgorithm());
+        System.arraycopy(other.state, 0, state, 0, other.state.length);
+        this.digestSizeBits = other.digestSizeBits;
+        this.rateSizeBits = other.rateSizeBits;
+
+        this.digestSizeBytes = digestSizeBits >>> 3;
+        this.rateSizeWords = rateSizeBits >>> 6;
+
+        this.rateBits = other.rateBits;
+    }
+
+    public int rateSize() {
+        return rateSizeBits >>> 3;
+    }
+
+    protected int rateSizeBitsFor(int digestSizeBits) {
+        switch (digestSizeBits) {
+        case 128: return 1344;
+        case 224: return 1152;
+        case 256: return 1088;
+        case 288: return 1024;
+        case 384: return  832;
+        case 512: return  576;
+        default: throw new IllegalArgumentException("Invalid digestSizeBits: " + digestSizeBits + " ⊄ { 128, 224, 256, 288, 384, 512 }");
+        }
+    }
+
+    @Override
+    protected void engineReset() {
+
+        for (int i = 0; i < MAX_STATE_SIZE_WORDS; i++) {
+            state[i] = 0L;
+        }
+
+        rateBits = 0;
+        out = null;
+    }
+
+    @Override
+    protected int engineGetDigestLength() {
+        return digestSizeBytes;
+    }
+
+    @Override
+    protected void engineUpdate(byte input) {
+        updateBits(input & 0xFFL, 8);
+    }
+
+    @Override
+    protected void engineUpdate(byte[] input, int offset, int len) {
+        engineUpdate(ByteBuffer.wrap(input, offset, len));
+    }
+
+    @Override
+    protected void engineUpdate(ByteBuffer in) {
+
+        int remaining = in.remaining();
+        if (remaining <= 0) {
+            return;
+        }
+
+        int rateBits = this.rateBits;
+        if ((rateBits & 0b111) > 0) {
+            throw new IllegalStateException("Cannot update while in bit mode");
+        }
+
+        long[] state = this.state;
+        int rateBytes = rateBits >>> 3;
+
+        int rateBytesWord = rateBytes & 0b111;
+        if (rateBytesWord > 0) {
+            int c = 8 - rateBytesWord;
+            if (c > remaining)
+                c = remaining;
+            int i = rateBytes >>> 3;
+            long w = state[i];
+            rateBytes += c;
+            remaining -= c;
+            rateBytesWord <<= 3;
+            c = rateBytesWord + (c << 3);
+
+            do {
+                w ^= (in.get() & 0xFFL) << rateBytesWord;
+                rateBytesWord += 8;
+            } while (rateBytesWord < c);
+
+            state[i] = w;
+            this.rateBits = rateBytes << 3;
+            if (remaining <= 0) {
+                return;
+            }
+        }
+
+        int rateWords = rateBytes >>> 3;
+        int inWords = remaining >>> 3;
+        if (inWords > 0) {
+            ByteOrder order = in.order();
+            try {
+                in.order(ByteOrder.LITTLE_ENDIAN);
+                do {
+                    if (rateWords >= rateSizeWords) {
+                        keccak(state);
+                        rateWords = 0;
+                    }
+                    int c = rateSizeWords - rateWords;
+                    if (c > inWords)
+                        c = inWords;
+                    inWords -= c;
+                    c += rateWords;
+                    do {
+                        state[rateWords++] ^= in.getLong();
+                    } while (rateWords < c);
+                } while (inWords > 0);
+            } finally {
+                in.order(order);
+            }
+            this.rateBits = rateWords << 6;
+            remaining &= 0b111;
+        }
+
+        if (rateWords >= rateSizeWords) {
+            keccak(state);
+            this.rateBits = 0;
+            rateWords = 0;
+        }
+
+        if (remaining > 0) {
+            long w = state[rateWords];
+            final int remainingBits = remaining << 3; // remaining in [1, 7]
+            int shiftAmount = 0;
+            switch (remaining) {
+            case 7: w ^= in.get() & 0xFFL; shiftAmount = Byte.SIZE;
+            case 6: w ^= (in.get() & 0xFFL) << shiftAmount; shiftAmount += Byte.SIZE;
+            case 5: w ^= (in.get() & 0xFFL) << shiftAmount; shiftAmount += Byte.SIZE;
+            case 4: w ^= (in.get() & 0xFFL) << shiftAmount; shiftAmount += Byte.SIZE;
+            case 3: w ^= (in.get() & 0xFFL) << shiftAmount; shiftAmount += Byte.SIZE;
+            case 2: w ^= (in.get() & 0xFFL) << shiftAmount; shiftAmount += Byte.SIZE;
+            case 1: w ^= (in.get() & 0xFFL) << shiftAmount;
+            }
+
+            state[rateWords] = w;
+            this.rateBits += remainingBits;
+        }
+    }
+
+    @Override
+    protected int engineDigest(byte[] buf, int offset, int len) {
+        out = ByteBuffer.wrap(buf, offset, len);
+        engineDigest();
+        return len;
+    }
+
+    @Override
+    protected byte[] engineDigest() {
+
+        pad();
+
+        int remaining;
+        if(out != null) {
+            remaining = out.remaining();
+        } else {
+            out = ByteBuffer.allocate(digestSizeBytes);
+            remaining = digestSizeBytes;
+        }
+
+        int rateWords = 0;
+        int outWords = remaining >>> 3;
+        if (outWords > 0) {
+            out.order(ByteOrder.LITTLE_ENDIAN);
+            do {
+                if (rateWords >= rateSizeWords) {
+                    keccak(state); // squeeze
+                    rateWords = 0;
+                }
+                int c = rateSizeWords - rateWords;
+                if (c > outWords)
+                    c = outWords;
+                outWords -= c;
+                c += rateWords;
+                do {
+                    out.putLong(state[rateWords]);
+                    rateWords++;
+                } while (rateWords < c);
+            } while (outWords > 0);
+            remaining &= 0b111;
+        }
+
+        if (remaining > 0) {
+            if (rateWords >= rateSizeWords) {
+                keccak(state); // squeeze
+                rateWords = 0;
+            }
+            long w = state[rateWords];
+
+            int shiftAmount = 0;
+            switch (remaining) {
+            case 7: out.put((byte) w); shiftAmount = Byte.SIZE;
+            case 6: out.put((byte) (w >>> shiftAmount)); shiftAmount += Byte.SIZE;
+            case 5: out.put((byte) (w >>> shiftAmount)); shiftAmount += Byte.SIZE;
+            case 4: out.put((byte) (w >>> shiftAmount)); shiftAmount += Byte.SIZE;
+            case 3: out.put((byte) (w >>> shiftAmount)); shiftAmount += Byte.SIZE;
+            case 2: out.put((byte) (w >>> shiftAmount)); shiftAmount += Byte.SIZE;
+            case 1: out.put((byte) (w >>> shiftAmount));
+            }
+        }
+
+        try {
+            return out.array();
+        } finally {
+            engineReset();
+        }
+    }
+
+    protected void pad() {
+        updateBits(0x1L, 1);
+        if (rateBits >= rateSizeBits) {
+            keccak(state);
+        }
+        rateBits = rateSizeBits - 1;
+        updateBits(0x1L, 1);
+        keccak(state);
+    }
+
+    protected void updateBits(long in, int inBits) {
+
+//        if (inBits < 0 || inBits > 64)
+//            throw new IllegalArgumentException("Invalid valueBits: " + 0 + " < " + inBits + " > " + 64);
+
+//        if (inBits <= 0)
+//            return;
+
+        int rateBits = this.rateBits;
+        int rateBitsWord = rateBits & 0x3f;
+        if (rateBitsWord > 0) {
+            int c = 64 - rateBitsWord;
+            if (c > inBits)
+                c = inBits;
+            state[rateBits >>> 6] ^= (in & (-1L >>> c)) << rateBitsWord;
+            rateBits += c;
+            inBits -= c;
+            if (inBits <= 0) {
+                this.rateBits = rateBits;
+                return;
+            }
+            in >>>= c;
+        }
+        if (rateBits >= rateSizeBits) {
+            keccak(state);
+//            rateBits = 0;
+            state[0] ^= in & (-1L >>> inBits);
+            this.rateBits = inBits;
+            return;
+        }
+        state[rateBits >>> 6] ^= in & (-1L >>> inBits);
+        this.rateBits = rateBits + inBits;
+    }
+
+    private static void keccak(long[] a) {
+        int c, i;
+        long x, a_10_;
+        long x0, x1, x2, x3, x4;
+        long t0, t1, t2, t3, t4;
+        long c0, c1, c2, c3, c4;
+
+        i = 0;
+        do {
+            // theta (precalculation part)
+            c0 = a[0] ^ a[5 + 0] ^ a[10 + 0] ^ a[15 + 0] ^ a[20 + 0];
+            c1 = a[1] ^ a[5 + 1] ^ a[10 + 1] ^ a[15 + 1] ^ a[20 + 1];
+            c2 = a[2] ^ a[5 + 2] ^ a[10 + 2] ^ a[15 + 2] ^ a[20 + 2];
+            c3 = a[3] ^ a[5 + 3] ^ a[10 + 3] ^ a[15 + 3] ^ a[20 + 3];
+            c4 = a[4] ^ a[5 + 4] ^ a[10 + 4] ^ a[15 + 4] ^ a[20 + 4];
+
+            t0 = (c0 << 1) ^ (c0 >>> (64 - 1)) ^ c3;
+            t1 = (c1 << 1) ^ (c1 >>> (64 - 1)) ^ c4;
+            t2 = (c2 << 1) ^ (c2 >>> (64 - 1)) ^ c0;
+            t3 = (c3 << 1) ^ (c3 >>> (64 - 1)) ^ c1;
+            t4 = (c4 << 1) ^ (c4 >>> (64 - 1)) ^ c2;
+
+            // theta (xorring part) + rho + pi
+            a[ 0] ^= t1;
+            x = a[ 1] ^ t2; a_10_ = (x <<  1) | (x >>> (64 -  1));
+            x = a[ 6] ^ t2; a[ 1] = (x << 44) | (x >>> (64 - 44));
+            x = a[ 9] ^ t0; a[ 6] = (x << 20) | (x >>> (64 - 20));
+            x = a[22] ^ t3; a[ 9] = (x << 61) | (x >>> (64 - 61));
+
+            x = a[14] ^ t0; a[22] = (x << 39) | (x >>> (64 - 39));
+            x = a[20] ^ t1; a[14] = (x << 18) | (x >>> (64 - 18));
+            x = a[ 2] ^ t3; a[20] = (x << 62) | (x >>> (64 - 62));
+            x = a[12] ^ t3; a[ 2] = (x << 43) | (x >>> (64 - 43));
+            x = a[13] ^ t4; a[12] = (x << 25) | (x >>> (64 - 25));
+
+            x = a[19] ^ t0; a[13] = (x <<  8) | (x >>> (64 -  8));
+            x = a[23] ^ t4; a[19] = (x << 56) | (x >>> (64 - 56));
+            x = a[15] ^ t1; a[23] = (x << 41) | (x >>> (64 - 41));
+            x = a[ 4] ^ t0; a[15] = (x << 27) | (x >>> (64 - 27));
+            x = a[24] ^ t0; a[ 4] = (x << 14) | (x >>> (64 - 14));
+
+            x = a[21] ^ t2; a[24] = (x <<  2) | (x >>> (64 -  2));
+            x = a[ 8] ^ t4; a[21] = (x << 55) | (x >>> (64 - 55));
+            x = a[16] ^ t2; a[ 8] = (x << 45) | (x >>> (64 - 45));
+            x = a[ 5] ^ t1; a[16] = (x << 36) | (x >>> (64 - 36));
+            x = a[ 3] ^ t4; a[ 5] = (x << 28) | (x >>> (64 - 28));
+
+            x = a[18] ^ t4; a[ 3] = (x << 21) | (x >>> (64 - 21));
+            x = a[17] ^ t3; a[18] = (x << 15) | (x >>> (64 - 15));
+            x = a[11] ^ t2; a[17] = (x << 10) | (x >>> (64 - 10));
+            x = a[ 7] ^ t3; a[11] = (x <<  6) | (x >>> (64 -  6));
+            x = a[10] ^ t1; a[ 7] = (x <<  3) | (x >>> (64 -  3));
+            a[10] = a_10_;
+
+            // chi
+            c = 0;
+            do {
+                x0 = a[c]; x1 = a[c + 1]; x2 = a[c + 2]; x3 = a[c + 3]; x4 = a[c + 4];
+                a[c] = x0 ^ ((~x1) & x2);
+                a[c + 1] = x1 ^ ((~x2) & x3);
+                a[c + 2] = x2 ^ ((~x3) & x4);
+                a[c + 3] = x3 ^ ((~x4) & x0);
+                a[c + 4] = x4 ^ ((~x0) & x1);
+
+                c += 5;
+            } while (c < 25);
+
+            // iota
+            switch (i) {
+            case 0: a[0] ^= 0x0000000000000001L; break;
+            case 1: a[0] ^= 0x0000000000008082L; break;
+            case 2: a[0] ^= 0x800000000000808AL; break;
+            case 3: a[0] ^= 0x8000000080008000L; break;
+            case 4: a[0] ^= 0x000000000000808BL; break;
+
+            case 5: a[0] ^= 0x0000000080000001L; break;
+            case 6: a[0] ^= 0x8000000080008081L; break;
+            case 7: a[0] ^= 0x8000000000008009L; break;
+            case 8: a[0] ^= 0x000000000000008AL; break;
+            case 9: a[0] ^= 0x0000000000000088L; break;
+
+            case 10: a[0] ^= 0x0000000080008009L; break;
+            case 11: a[0] ^= 0x000000008000000AL; break;
+            case 12: a[0] ^= 0x000000008000808BL; break;
+            case 13: a[0] ^= 0x800000000000008BL; break;
+            case 14: a[0] ^= 0x8000000000008089L; break;
+
+            case 15: a[0] ^= 0x8000000000008003L; break;
+            case 16: a[0] ^= 0x8000000000008002L; break;
+            case 17: a[0] ^= 0x8000000000000080L; break;
+            case 18: a[0] ^= 0x000000000000800AL; break;
+            case 19: a[0] ^= 0x800000008000000AL; break;
+
+            case 20: a[0] ^= 0x8000000080008081L; break;
+            case 21: a[0] ^= 0x8000000000008080L; break;
+            case 22: a[0] ^= 0x0000000080000001L; break;
+            case 23: a[0] ^= 0x8000000080008008L;
+            }
+
+            i++;
+        } while (i < 24);
+    }
+}
--- src/test/java/com/esaulpaugh/headlong/rlp/Benchmark.java
+++ src/test/java/com/esaulpaugh/headlong/rlp/Benchmark.java
@@ -2,10 +2,19 @@ package com.esaulpaugh.headlong.rlp;
 
 import com.esaulpaugh.headlong.rlp.example.Student;
 import com.esaulpaugh.headlong.rlp.util.ObjectNotation;
+import com.joemelsha.crypto.hash.Keccak;
+import org.junit.Assert;
 import org.junit.Test;
+import org.spongycastle.crypto.Digest;
+import org.spongycastle.crypto.digests.KeccakDigest;
+import org.spongycastle.jcajce.provider.digest.Keccak.DigestKeccak;
 import org.spongycastle.util.encoders.Hex;
 
+import java.security.DigestException;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.text.DecimalFormat;
+import java.util.Random;
 
 import static com.esaulpaugh.headlong.rlp.RLPDecoder.RLP_LENIENT;
 
@@ -14,9 +23,85 @@ public class Benchmark {
     private static final byte[] STUDENT_RLP_SEQUENTIAL = Hex.decode("85506c61746f84460ca00ab88a3232b0883839e5de6a8bf0555b6304b703041e82fe7568aa8b6837aa62740a83fe5aaa8736a1c2a27080f77142702cdf4a81ca2744bda44397bbd58c63f35c0eb6796bf485d750a0b9bfa4a2f3be5b9030a7f2b13d6a4d468e22b32fe92506b11af5517d425bc68f26f2525a61f1a954c50933874c7d97b1cd8ff65d55f651cb7c455876278787ac3a40b4269da3281d03da142f61ba27534caabf68f4cf30bcd23399b8cdf6fddc601f76012819f4572f901661ec6a5122f901661ec6a51279");
 
     @Test
-    public void decodeMicroBenchmark() throws DecodeException {
+    public void decodeMicroBenchmark() throws DecodeException, DigestException {
         Student plato;
 //        StudentRLPAdapter adapter = new StudentRLPAdapter();
+
+        Random r = new Random(new SecureRandom().nextLong());
+
+        for (int i = 20000; i >= 0; i--) {
+
+            byte[] input = new byte[i];
+            r.nextBytes(input);
+
+            if(i % 1000 == 0) System.out.println(input.length);
+
+            String prev, current;
+
+            byte[] output;
+
+            MessageDigest k0 = new Keccak(256);
+            final int random = r.nextInt(input.length + 1);
+            k0.update(input, 0, random);
+            k0.update(input, random, input.length - random);
+//            k0.update(Arrays.copyOfRange(input, 0, random));
+//            k0.update(Arrays.copyOfRange(input, random, input.length));
+            output = k0.digest();
+            current = Hex.toHexString(output);
+//            System.out.println(current);
+            prev = current;
+
+            output = new Keccak(256).digest(input);
+            current = Hex.toHexString(output);
+//            System.out.println(current);
+            Assert.assertEquals(prev, current);
+            prev = current;
+
+            MessageDigest k = new Keccak(256);
+            k.update(input);
+            output = k.digest();
+            current = Hex.toHexString(output);
+//            System.out.println(current);
+            Assert.assertEquals(prev, current);
+            prev = current;
+
+            MessageDigest _256no = new Keccak(256);
+            _256no.update(input);
+            _256no.digest(output, 0, output.length);
+            current = Hex.toHexString(output);
+//            System.out.println(current);
+            Assert.assertEquals(prev, current);
+            prev = current;
+
+//            System.out.println();
+
+            MessageDigest md = new org.bouncycastle.jcajce.provider.digest.Keccak.Digest256();
+            md.update(input);
+            md.digest(output, 0, output.length);
+            current = Hex.toHexString(output);
+//            System.out.println(current);
+            Assert.assertEquals(prev, current);
+            prev = current;
+
+            MessageDigest digestKeccak = new DigestKeccak(256);
+            digestKeccak.update(input);
+            digestKeccak.digest(output, 0, output.length);
+            current = Hex.toHexString(output);
+//            System.out.println(current);
+            Assert.assertEquals(prev, current);
+            prev = current;
+
+            Digest spongy = new KeccakDigest(256);
+            spongy.update(input, 0, input.length);
+            spongy.doFinal(output, 0);
+            current = Hex.toHexString(output);
+//            System.out.println(current);
+            Assert.assertEquals(prev, current);
+            prev = current;
+
+        }
+
+        if(true)return;
 
         byte[] rlp = STUDENT_RLP_SEQUENTIAL;
 //        byte[] temp = new byte[rlp.length];
