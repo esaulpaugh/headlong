b075a335ae527e7c91778e971ed6e74389d4b247
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2019-01-04 18:30:30 -0600

    remove ShortType and ABIUtils

--- src/main/java/com/esaulpaugh/headlong/util/Utils.java
+++ src/main/java/com/esaulpaugh/headlong/util/Utils.java
@@ -4,4 +4,15 @@ public class Utils {
 
     public static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
 
+//    public static int convertPos(ByteBuffer bb) {
+//        return (bb.position() - 4) >>> 5;
+//    }
+//
+//    public static int convert(int pos) {
+//        return convertOffset(pos - 4);
+//    }
+//
+//    public static int convertOffset(int pos) {
+//        return pos >>> 5;
+//    }
 }
