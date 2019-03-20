c089c2d4d2fef2eaffb0c9bb706a1c991b090a32
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2019-03-20 03:37:00 -0500

    code cleanup

--- src/test/java/com/esaulpaugh/headlong/rlp/EIP778Test.java
+++ src/test/java/com/esaulpaugh/headlong/rlp/EIP778Test.java
@@ -1,18 +1,22 @@
 package com.esaulpaugh.headlong.rlp;
 
 import com.esaulpaugh.headlong.util.FastHex;
-import com.esaulpaugh.headlong.util.Strings;
 import org.junit.Test;
 
 import java.util.Random;
 
 import static com.esaulpaugh.headlong.rlp.EIP778.*;
 
+import static com.esaulpaugh.headlong.util.Strings.HEX;
+import static com.esaulpaugh.headlong.util.Strings.UTF_8;
+
 public class EIP778Test {
 
     @Test
     public void testEip778() throws DecodeException {
+
         long seq = 6L;
+
         EIP778.KeyValuePair[] pairs = new EIP778.KeyValuePair[] {
                 new EIP778.KeyValuePair(IP, "192.168.0.7"),
                 new EIP778.KeyValuePair(UDP, "30301"),
@@ -34,17 +38,16 @@ public class EIP778Test {
         System.out.println("record len = " + record.length);
         System.out.println("record = " + FastHex.encodeToString(record) + '\n');
 
-        RLPListIterator listIter = RLPDecoder.RLP_STRICT.listIterator(record);
+        RLPListIterator iter = RLPDecoder.RLP_STRICT.listIterator(record);
 
-        System.out.println("signature = " + listIter.next().asString(Strings.HEX));
+        System.out.println("signature = " + iter.next().asString(HEX));
 
-        RLPSequenceIterator seqIter = RLPDecoder.RLP_STRICT.sequenceIterator(listIter.next().data());
+        RLPSequenceIterator iter2 = RLPDecoder.RLP_STRICT.sequenceIterator(iter.next().data());
 
-        System.out.println("seq = " + seqIter.next().asLong());
+        System.out.println("seq = " + iter2.next().asLong());
 
-        while (seqIter.hasNext()) {
-            System.out.println(seqIter.next().asString(Strings.UTF_8) + ", "
-                    + seqIter.next().asString(Strings.UTF_8));
+        while (iter2.hasNext()) {
+            System.out.println(iter2.next().asString(UTF_8) + ", " + iter2.next().asString(UTF_8));
         }
     }
 }
