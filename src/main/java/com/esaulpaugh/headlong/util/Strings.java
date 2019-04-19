fe7761fedf5087ef0bbbd546e8521646dc2b12d8
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2019-04-19 06:07:31 -0500

    remove base64 decoding
    switch base64 encoder to migbase64 (BSD license);
    jar size reduced to 116KiB

--- src/main/java/com/esaulpaugh/headlong/util/Strings.java
+++ src/main/java/com/esaulpaugh/headlong/util/Strings.java
@@ -1,9 +1,10 @@
 package com.esaulpaugh.headlong.util;
 
+import com.migcomponents.migbase64.Base64;
+
 import java.nio.charset.Charset;
 
 import static com.esaulpaugh.headlong.util.Utils.EMPTY_BYTE_ARRAY;
-import java.io.IOException;
 
 /**
  * Utility for encoding and decoding hexadecimal, Base64, and UTF-8-encoded {@code String}s.
@@ -17,8 +18,8 @@ public class Strings {
     public static final int BASE64 = 1; // 64
     public static final int HEX = 2; // 16
 
-    public static final boolean WITH_PADDING = true;
-    public static final boolean NO_PADDING = false;
+    public static final boolean PAD = true;
+    public static final boolean DONT_PAD = false;
 
     public static String encode(byte[] bytes) {
         return FastHex.encodeToString(bytes, 0, bytes.length);
@@ -35,7 +36,7 @@ public class Strings {
     public static String encode(byte[] bytes, int from, int len, int encoding) {
         switch (encoding) {
         case UTF_8: return new String(bytes, from, len, CHARSET_UTF_8);
-        case BASE64: return toBase64(bytes, from, len, WITH_PADDING);
+        case BASE64: return toBase64(bytes, from, len, PAD);
         case HEX:
         default: return FastHex.encodeToString(bytes, from, len);
         }
@@ -44,15 +45,10 @@ public class Strings {
     public static byte[] decode(String string, int encoding) {
         switch (encoding) {
         case UTF_8: return fromUtf8(string);
-        case BASE64: return fromBase64(string, WITH_PADDING);
+        case BASE64: throw new UnsupportedOperationException(); // decoding no longer supported. try spongycastle
         case HEX:
         default: return fromHex(string);
         }
-    }
-
-    public static String toBase64(byte[] bytes, int from, int len, boolean withPadding) {
-        final int options = withPadding ? Base64.NO_OPTIONS : Base64.NO_PADDING;
-        return Base64.encodeBytes(bytes, from, len, options);
     }
 
     private static byte[] fromUtf8(String utf8) {
@@ -62,90 +58,15 @@ public class Strings {
         return utf8.getBytes(CHARSET_UTF_8);
     }
 
+    public static String toBase64(byte[] bytes, int from, int len, boolean pad) {
+//        final int options = withPadding ? Base64.NO_OPTIONS : Base64.NO_PADDING;
+        return Base64.encodeToString(bytes, from, len, false, pad);
+    }
+
     private static byte[] fromHex(String hex) {
         if(hex.isEmpty()) {
             return EMPTY_BYTE_ARRAY;
         }
         return FastHex.decode(hex, 0 ,hex.length());
     }
-
-    public static byte[] fromBase64(String base64, boolean hasPadding) {
-        if (base64.isEmpty()) {
-            return EMPTY_BYTE_ARRAY;
-        }
-        final int options = hasPadding ? Base64.NO_OPTIONS : Base64.NO_PADDING;
-        try {
-            return Base64.decode(base64, options);
-        } catch (IOException io) {
-            throw new IllegalArgumentException(io);
-        }
-    }
-
-//    public static int calcHexDecodedLen(int encodedLen) {
-//        return encodedLen >> 1; // div by two
-//    }
-//
-//    public static int calcBase64DecodedLen(String base64) {
-//        return (int) Math.floor(getUnpaddedLen(base64) * 0.75f); // ⌊⌋
-//    }
-//
-//    public static int calcDecodedLen(String string, int encoding) {
-//        switch (encoding) {
-//        case UTF_8: return string.getBytes(CHARSET_UTF_8).length;
-//        case BASE64: return calcBase64DecodedLen(string);
-//        case HEX:
-//        default: return calcHexDecodedLen(string.length());
-//        }
-//    }
-//
-//    private static int getUnpaddedLen(String base64) {
-//        final int len = base64.length();
-//        if(len == 0) return 0;
-//        if (base64.charAt(len - 1) != '=')
-//            return len;
-//        int temp;
-//        return base64.charAt((temp = len - 2)) == '='
-//                ? temp
-//                : len - 1;
-//    }
-//
-//    static String unpadBase64(byte[] padded) { // ascii bytes
-//        final int len = padded.length;
-//        if(len < 4) {
-//            return "";
-//        }
-//        int last = len - 1;
-//        if(padded[last] != '=') {
-//            return new String(padded, CHARSET_UTF_8);
-//        }
-//        int secondToLast = len - 2;
-//        final int newLen = padded[secondToLast] == '=' ? secondToLast : last;
-//
-//        return new String(padded, 0, newLen, CHARSET_UTF_8);
-//    }
-//
-//    static byte[] padBase64(String unpadded) {
-//        final int unpaddedLen = unpadded.length();
-//        final int remainder = unpadded.length() % 4;
-//        switch (remainder) {
-//        case 0: return unpadded.getBytes(CHARSET_UTF_8);
-//        case 1: break;
-//        case 2: {
-//            final int paddedLen = unpaddedLen + 2;
-//            byte[] bytes = new byte[paddedLen];
-//            bytes[paddedLen - 2] = '=';
-//            bytes[paddedLen - 1] = '=';
-//            unpadded.getBytes(0, unpaddedLen, bytes, 0); // there is no replacement for this method
-//            return bytes;
-//        }
-//        case 3: {
-//            final int paddedLen = unpaddedLen + 1;
-//            byte[] bytes = new byte[paddedLen];
-//            bytes[paddedLen - 1] = '=';
-//            unpadded.getBytes(0, unpaddedLen, bytes, 0); // there is no replacement for this method
-//            return bytes;
-//        }
-//        }
-//        throw new IllegalArgumentException("illegal input length: " + unpaddedLen);
-//    }
 }
--- src/test/java/com/esaulpaugh/headlong/util/StringsTest.java
+++ src/test/java/com/esaulpaugh/headlong/util/StringsTest.java
@@ -1,5 +1,6 @@
 package com.esaulpaugh.headlong.util;
 
+import com.esaulpaugh.headlong.TestUtils;
 import com.esaulpaugh.headlong.abi.MonteCarloTest;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,16 +10,6 @@ import java.util.Random;
 import static com.esaulpaugh.headlong.util.Strings.*;
 
 public class StringsTest {
-
-    private void randomTest(Random rand, int encoding) {
-        for (int i = 0; i < 20_000; i++) {
-            byte[] x = new byte[rand.nextInt(400)]; // rand.nextInt(14)
-            rand.nextBytes(x);
-            String s = Strings.encode(x, encoding);
-            byte[] y = Strings.decode(s, encoding);
-            Assert.assertArrayEquals(x, y);
-        }
-    }
 
     @Test
     public void utf8() {
@@ -37,26 +28,61 @@ public class StringsTest {
     }
 
     @Test
-    public void base64() {
+    public void hex() {
         Random rand = new Random(MonteCarloTest.getSeed(System.nanoTime()));
-        randomTest(rand, BASE64);
+        for (int i = 0; i < 20_000; i++) {
+            byte[] x = new byte[rand.nextInt(400)]; // rand.nextInt(14)
+            rand.nextBytes(x);
+            String s = Strings.encode(x, HEX);
+            byte[] y = Strings.decode(s, HEX);
+            Assert.assertArrayEquals(x, y);
+        }
     }
 
     @Test
-    public void hex() {
+    public void padding() {
         Random rand = new Random(MonteCarloTest.getSeed(System.nanoTime()));
-        randomTest(rand, HEX);
+        for (int i = 0; i < 20_000; i++) {
+            byte[] x = new byte[rand.nextInt(400)]; // rand.nextInt(14)
+            rand.nextBytes(x);
+            String s = Strings.encode(x, BASE64);
+            Assert.assertEquals(encodedLen(x.length, true), s.length());
+        }
     }
 
     @Test
     public void noPadding() {
         Random rand = new Random(MonteCarloTest.getSeed(System.nanoTime()));
+        int sum = 0;
         for (int i = 0; i < 20_000; i++) {
-            byte[] x = new byte[rand.nextInt(400)]; // rand.nextInt(14)
+            byte[] x = new byte[3 + rand.nextInt(400)];
             rand.nextBytes(x);
-            String s = Strings.toBase64(x, 0, x.length, NO_PADDING);
-            byte[] y = Strings.fromBase64(s, NO_PADDING);
-            Assert.assertArrayEquals(x, y);
+            int offset = rand.nextInt(x.length / 3);
+            int len = rand.nextInt(x.length / 2);
+            String s = com.migcomponents.migbase64.Base64.encodeToString(x, offset, len, false, DONT_PAD);
+            Assert.assertEquals(encodedLen(len, false), s.length());
         }
+        rand.setSeed(sum);
+    }
+
+    @Test
+    public void tryDecodeBase64() throws Throwable {
+        TestUtils.assertThrown(UnsupportedOperationException.class, () -> Strings.decode("", BASE64));
+    }
+
+    private static int encodedLen(int numBytes, boolean padding) {
+        if(padding) {
+            return numBytes / 3 * 4 + (numBytes % 3 > 0 ? 4 : 0);
+        }
+//        return (int) StrictMath.ceil(inputLen * 4 / 3d);
+        int estimated = numBytes / 3 * 4;
+        int mod = numBytes % 3;
+        if(mod == 0) {
+            return estimated;
+        }
+        if(mod == 1) {
+            return estimated + 2;
+        }
+        return estimated + 3;
     }
 }
