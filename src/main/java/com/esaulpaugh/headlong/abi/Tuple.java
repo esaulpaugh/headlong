34fe2578e54517b920b87993ae57591a9029167b
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2020-02-07 04:44:35 -0600

    change version to 3.0.0-SNAPSHOT;
    add ant property project.version

--- src/main/java/com/esaulpaugh/headlong/abi/Tuple.java
+++ src/main/java/com/esaulpaugh/headlong/abi/Tuple.java
@@ -23,7 +23,7 @@ import java.util.RandomAccess;
 /**
  * An ordered list of objects whose types should correspond to some {@link TupleType}. {@link Function}s encode/decode
  * {@link Tuple}s containing arguments satisfying its parameters/return type. {@link Tuple}s can contain other tuples.
- * Elements not safe from external or concurrent modification.
+ * Elements array is not safe from external/concurrent modification.
  */
 public final class Tuple extends AbstractList<Object> implements RandomAccess, Serializable {
 
