7e41f6a3700e0b2d0b3e9391009cc2fb6c427d16
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2020-07-13 04:41:48 -0500

    use shallow defensive copy in Tuple for better immutability

--- src/main/java/com/esaulpaugh/headlong/abi/Function.java
+++ src/main/java/com/esaulpaugh/headlong/abi/Function.java
@@ -216,7 +216,6 @@ public final class Function implements ABIObject {
         return Function.SELECTOR_LEN + inputTypes.measureEncodedLength(args);
     }
 
-    /**@param args  {@link Tuple#Tuple(Object...)} does not make a defensive copy */
     public ByteBuffer encodeCallWithArgs(Object... args) {
         return encodeCall(new Tuple(args));
     }
