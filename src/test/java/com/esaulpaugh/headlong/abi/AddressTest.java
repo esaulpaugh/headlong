cafbb5190a4b9988555394eddf366e62a52ec937
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2021-10-30 19:52:54 -0500

    reduce test iterations in AddressTest; add test; split test;

--- src/test/java/com/esaulpaugh/headlong/abi/AddressTest.java
+++ src/test/java/com/esaulpaugh/headlong/abi/AddressTest.java
@@ -23,25 +23,34 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Random;
 
+import static com.esaulpaugh.headlong.TestUtils.assertThrown;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AddressTest {
 
+    private static final String[] VECTORS = new String[] {
+            "0x52908400098527886E0F7030069857D2E4169EE7",
+            "0x8617E340B3D01FA5F11F306F4090FD50E238070D",
+            "0xde709f2102306220921060314715629080e2fb77",
+            "0x27b1fdb04752bbc536007a920d24acb045561c26",
+            "0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed",
+            "0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359",
+            "0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB",
+            "0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb" };
+
     @Test
     public void testVectorChecksums() {
-        String[] valid = new String[] {
-                "0x52908400098527886E0F7030069857D2E4169EE7",
-                "0x8617E340B3D01FA5F11F306F4090FD50E238070D",
-                "0xde709f2102306220921060314715629080e2fb77",
-                "0x27b1fdb04752bbc536007a920d24acb045561c26",
-                "0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed",
-                "0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359",
-                "0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB",
-                "0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb" };
-
-        for(String address : valid) {
+        for(String address : VECTORS) {
             Address.validateChecksumAddress(address);
+        }
+    }
+
+    @Test
+    public void testCorruptedVectors() throws Throwable {
+        for(String address : VECTORS) {
+            final String corrupted = address.replace('F', 'f').replace('b', 'B');
+            assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.validateChecksumAddress(corrupted));
         }
     }
 
@@ -72,11 +81,11 @@ public class AddressTest {
         testBigIntAddr(Address.wrap("0x0000000005CaFEbabeCafEbABE7570ad8ac11F8d").value());
         testBigIntAddr(Address.wrap("0x0000000000000000000082095CafEBABEcAFebAB").value());
 
-        TestUtils.assertThrown(IllegalArgumentException.class,
+        assertThrown(IllegalArgumentException.class,
                 "invalid bit length: 161",
                 () -> Address.toChecksumAddress(new BigInteger("182095cafebabecafebabe00083ce15d74e191051", 16))
         );
-        TestUtils.assertThrown(IllegalArgumentException.class,
+        assertThrown(IllegalArgumentException.class,
                 "invalid bit length: 164",
                 () -> Address.toChecksumAddress(new BigInteger("82095cafebabecafebabe00083ce15d74e1910510", 16))
         );
@@ -84,22 +93,21 @@ public class AddressTest {
         final SecureRandom sr = new SecureRandom();
         sr.setSeed(new SecureRandom().generateSeed(64));
         sr.setSeed(sr.generateSeed(64));
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 10; i++) {
             testBigIntAddr(new BigInteger(TypeFactory.ADDRESS_BIT_LEN, sr));
         }
 
         final Random r = new Random(sr.nextLong());
         for (int bitlen = 0; bitlen <= 160; bitlen++) {
-            for (int i = 0; i < 10; i++) {
-                testBigIntAddr(new BigInteger(bitlen, r));
-            }
+            testBigIntAddr(new BigInteger(bitlen, r));
+            testBigIntAddr(new BigInteger(bitlen, r));
         }
         BigInteger temp;
         do {
             temp = new BigInteger(161, r);
         } while (temp.bitLength() < 161);
         final BigInteger tooBig = temp;
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid bit length: 161", () -> Address.toChecksumAddress(tooBig));
+        assertThrown(IllegalArgumentException.class, "invalid bit length: 161", () -> Address.toChecksumAddress(tooBig));
     }
 
     @Test
@@ -108,53 +116,10 @@ public class AddressTest {
         testStringAddr(Address.toChecksumAddress(BigInteger.ONE));
         testStringAddr(Address.toChecksumAddress(BigInteger.TEN));
         testStringAddr(Address.toChecksumAddress(BigInteger.valueOf(2L)));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x82095cafebabecafebabe00083ce15d74e191051"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x4bec173f8d9d3d90188777cafebabecafebabe99"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x4bec173f8d9d3d90188777CAFEBABEcafebabe99"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x5cafebabecafebabe7570ad8ac11f8d812ee0606"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x0000000005cafebabecafebabe7570ad8ac11f8d"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x0000000000000000000082095cafebabecafebab"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0xc0ec0fbb1c07aebe2a6975d50b5f6441b05023f9"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0xa62274005cafebabecafebabecaebb178db50ad6"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0xc6782c3a8155971a5d16005cafebabecafebabe8"));
-        TestUtils.assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x000000000000000000000000000000000000ffff"));
-
-        TestUtils.assertThrown(IllegalArgumentException.class,
-                "illegal hex val @ 2",
-                () -> Address.wrap("0x+000000000000000000082095cafebabecafebab")
-        );
-
-        TestUtils.assertThrown(IllegalArgumentException.class,
-                "illegal hex val @ 2",
-                () -> Address.wrap("0x-000000000000000000082095cafebabecafebab")
-        );
-
-        TestUtils.assertThrown(IllegalArgumentException.class,
-                "illegal hex val @ 41",
-                () -> Address.wrap("0x0000000000000000000082095cafebabecafeba+")
-        );
-
-        TestUtils.assertThrown(IllegalArgumentException.class,
-                "expected prefix 0x not found",
-                () -> Address.wrap("aaaaa")
-        );
-        TestUtils.assertThrown(IllegalArgumentException.class,
-                "expected prefix 0x not found",
-                () -> Address.wrap("5cafebabecafebabe7570ad8ac11f8d812ee0606")
-        );
-        TestUtils.assertThrown(IllegalArgumentException.class,
-                "expected address length 42; actual is 41",
-                () -> Address.wrap("0xa83aaef1b5c928162005cafebabecafebabecb0")
-        );
-        TestUtils.assertThrown(IllegalArgumentException.class,
-                "expected address length 42; actual is 43",
-                () -> Address.wrap("0xa83aaef1b5c928162005cafebabecafebabecb0a0")
-        );
 
         final Random r = TestUtils.seededRandom();
-        for (int i = 0; i < 1_000; i++) {
-            testStringAddr(MonteCarloTestCase.generateAddressString(r));
-        }
+        testStringAddr(MonteCarloTestCase.generateAddressString(r));
+        testStringAddr(MonteCarloTestCase.generateAddressString(r));
 
         BigInteger _FFff = Address.wrap("0x000000000000000000000000000000000000FFff").value();
         assertEquals(BigInteger.valueOf(65535L), _FFff);
@@ -204,6 +169,49 @@ public class AddressTest {
 
     private static BigInteger computeValue2(String checksumAddress) {
         return new BigInteger(checksumAddress.substring(2), 16);
+    }
+
+    @Test
+    public void testStringAddrExceptions() throws Throwable {
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x82095cafebabecafebabe00083ce15d74e191051"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x4bec173f8d9d3d90188777cafebabecafebabe99"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x4bec173f8d9d3d90188777CAFEBABEcafebabe99"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x5cafebabecafebabe7570ad8ac11f8d812ee0606"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x0000000005cafebabecafebabe7570ad8ac11f8d"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x0000000000000000000082095cafebabecafebab"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0xc0ec0fbb1c07aebe2a6975d50b5f6441b05023f9"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0xa62274005cafebabecafebabecaebb178db50ad6"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0xc6782c3a8155971a5d16005cafebabecafebabe8"));
+        assertThrown(IllegalArgumentException.class, "invalid checksum", () -> Address.wrap("0x000000000000000000000000000000000000ffff"));
+
+        assertThrown(IllegalArgumentException.class,
+                "illegal hex val @ 2",
+                () -> Address.wrap("0x+000000000000000000082095cafebabecafebab")
+        );
+        assertThrown(IllegalArgumentException.class,
+                "illegal hex val @ 2",
+                () -> Address.wrap("0x-000000000000000000082095cafebabecafebab")
+        );
+        assertThrown(IllegalArgumentException.class,
+                "illegal hex val @ 41",
+                () -> Address.wrap("0x0000000000000000000082095cafebabecafeba+")
+        );
+        assertThrown(IllegalArgumentException.class,
+                "expected prefix 0x not found",
+                () -> Address.wrap("aaaaa")
+        );
+        assertThrown(IllegalArgumentException.class,
+                "expected prefix 0x not found",
+                () -> Address.wrap("5cafebabecafebabe7570ad8ac11f8d812ee0606")
+        );
+        assertThrown(IllegalArgumentException.class,
+                "expected address length 42; actual is 41",
+                () -> Address.wrap("0xa83aaef1b5c928162005cafebabecafebabecb0")
+        );
+        assertThrown(IllegalArgumentException.class,
+                "expected address length 42; actual is 43",
+                () -> Address.wrap("0xa83aaef1b5c928162005cafebabecafebabecb0a0")
+        );
     }
 
     @Test
