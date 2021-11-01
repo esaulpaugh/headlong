9735ca29d7424a148a3798332aa435acbe581f56
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2021-11-01 00:31:07 -0500

    minor refactor

--- src/main/java/com/esaulpaugh/headlong/abi/Address.java
+++ src/main/java/com/esaulpaugh/headlong/abi/Address.java
@@ -128,7 +128,7 @@ public final class Address {
         switch (c) {
         case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':
         case 'a':case 'b':case 'c':case 'd':case 'e':case 'f': return (byte) c;
-        case 'A':case 'B':case 'C':case 'D':case 'E':case 'F': return (byte) Character.toLowerCase(c);
+        case 'A':case 'B':case 'C':case 'D':case 'E':case 'F': return (byte) (c + ('a' - 'A'));
         default: throw new IllegalArgumentException("illegal hex val @ " + i);
         }
     }
