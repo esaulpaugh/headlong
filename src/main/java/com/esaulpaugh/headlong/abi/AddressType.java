54dca25400fd5dde468dbdd91482290ab7fbba65
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2021-11-29 21:59:43 -0600

    make constant private

--- src/main/java/com/esaulpaugh/headlong/abi/AddressType.java
+++ src/main/java/com/esaulpaugh/headlong/abi/AddressType.java
@@ -20,7 +20,7 @@ import java.nio.ByteBuffer;
 /** The {@link ABIType} for {@link Address}. Corresponds to the "address" type. */
 public final class AddressType extends UnitType<Address> {
 
-    static final BigIntegerType ADDRESS_INNER = new BigIntegerType("ADDRESS_INNER", TypeFactory.ADDRESS_BIT_LEN, true);
+    private static final BigIntegerType ADDRESS_INNER = new BigIntegerType("ADDRESS_INNER", TypeFactory.ADDRESS_BIT_LEN, true);
 
     AddressType() {
         super("address", Address.class, TypeFactory.ADDRESS_BIT_LEN, true);
