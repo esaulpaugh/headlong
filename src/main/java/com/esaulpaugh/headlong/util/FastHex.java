881a1847123da7b66e3433f34e04972e9197922a
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2021-12-10 08:33:30 -0600

    format whitespace

--- src/main/java/com/esaulpaugh/headlong/util/FastHex.java
+++ src/main/java/com/esaulpaugh/headlong/util/FastHex.java
@@ -95,28 +95,9 @@ public final class FastHex {
 
     private static int decodeNibble(int c, int offset) {
         switch (c) {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9': return c - '0';
-        case 'A':
-        case 'B':
-        case 'C':
-        case 'D':
-        case 'E':
-        case 'F': return  c - ('A' - 0xA);
-        case 'a':
-        case 'b':
-        case 'c':
-        case 'd':
-        case 'e':
-        case 'f': return c - ('a' - 0xa);
+        case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9': return c - '0';
+        case 'A':case 'B':case 'C':case 'D':case 'E':case 'F': return  c - ('A' - 0xA);
+        case 'a':case 'b':case 'c':case 'd':case 'e':case 'f': return c - ('a' - 0xa);
         default: throw new IllegalArgumentException("illegal hex val @ " + offset);
         }
     }
