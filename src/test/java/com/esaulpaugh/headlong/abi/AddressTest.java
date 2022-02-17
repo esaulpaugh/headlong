547a1c683d8ca8d9af2adb554bbd6e63d09cedae
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2022-02-16 20:50:43 -0600

    add assertion

--- src/test/java/com/esaulpaugh/headlong/abi/AddressTest.java
+++ src/test/java/com/esaulpaugh/headlong/abi/AddressTest.java
@@ -305,5 +305,6 @@ public class AddressTest {
                         "Four score and seven years ago our fathers brought forth on this continent, a new nation, " +
                         "conceived in Liberty, and dedicated to the proposition that all men are created equal.")
         );
+        assertTrue(Address.MAX_LABEL_LEN < 40);
     }
 }
