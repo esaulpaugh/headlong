94ae1de21df9192c1490dab9426d625e3cdadd76
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2022-04-29 19:11:30 -0500

    eliminate wildcard imports

--- src/jmh/java/com/esaulpaugh/headlong/jmh/util/MeasureBase64.java
+++ src/jmh/java/com/esaulpaugh/headlong/jmh/util/MeasureBase64.java
@@ -17,7 +17,16 @@ package com.esaulpaugh.headlong.jmh.util;
 
 import com.esaulpaugh.headlong.util.FastBase64;
 import com.esaulpaugh.headlong.util.Strings;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.Random;
--- src/jmh/java/com/esaulpaugh/headlong/jmh/util/MeasureHex.java
+++ src/jmh/java/com/esaulpaugh/headlong/jmh/util/MeasureHex.java
@@ -16,7 +16,16 @@
 package com.esaulpaugh.headlong.jmh.util;
 
 import com.esaulpaugh.headlong.util.FastHex;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.Random;
