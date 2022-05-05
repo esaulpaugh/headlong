609ee1d4786e92c342e86123f255cbdbb4480f79
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2022-05-05 13:30:14 -0500

    allow all backwards jumps

--- src/main/java/com/esaulpaugh/headlong/abi/TupleType.java
+++ src/main/java/com/esaulpaugh/headlong/abi/TupleType.java
@@ -303,23 +303,11 @@ public final class TupleType extends ABIType<Tuple> implements Iterable<ABIType<
                 final int pos = bb.position();
                 if (jump != pos) {
                     /* LENIENT MODE; see https://github.com/ethereum/solidity/commit/3d1ca07e9b4b42355aa9be5db5c00048607986d1 */
-                    if (jump < pos && offsetNotShared(offset, offsets, i)) {
-                        throw new IllegalArgumentException("illegal backwards jump: (" + start + "+" + offset + "=" + jump + ")<" + pos);
-                    }
                     bb.position(jump); // leniently jump to specified offset
                 }
                 objects[i] = getType.apply(i).decode(bb, unitBuffer);
             }
         }
-    }
-
-    private static boolean offsetNotShared(int offset, int[] offsets, int i) {
-        for (int j = 0; j < i; j++) {
-            if (offsets[j] == offset) {
-                return false;
-            }
-        }
-        return true;
     }
 
     /**
