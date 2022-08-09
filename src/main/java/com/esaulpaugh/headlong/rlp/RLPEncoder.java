48eb78db6fe848622896a438f3902c7da9e84f93
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2022-08-09 09:36:45 -0500

    fix comment

--- src/main/java/com/esaulpaugh/headlong/rlp/RLPEncoder.java
+++ src/main/java/com/esaulpaugh/headlong/rlp/RLPEncoder.java
@@ -273,7 +273,7 @@ public final class RLPEncoder {
      * @param elements  the raw elements to be encoded as an RLP list item
      * @param dest      the destination for the encoded RLP list
      * @param destIndex the index into the destination for the list
-     * @return the index into {@code dest} marking the end of the sequence
+     * @return the index into {@code dest} marking the end of the list
      */
     public static int putList(Iterable<?> elements, byte[] dest, int destIndex) {
         ByteBuffer bb = ByteBuffer.wrap(dest, destIndex, dest.length - destIndex);
