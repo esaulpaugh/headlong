03f2e99de210bd61871c67675fd6fcbe1abdc548
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2023-03-24 00:47:15 -0500

    update jmh plugin in kts build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
 //plugins {
 //    id("java-library")
 //    id("maven-publish")
-//    id("me.champeau.jmh").version("0.6.8")
+//    id("me.champeau.jmh").version("0.7.0")
 //}
 //
 //group = "com.esaulpaugh"
