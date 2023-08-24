8017bdd8231b8700717332ba5e5120ff1cf3f322
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2023-08-23 22:54:21 -0500

    fix syntax error in test

--- src/test/java/com/esaulpaugh/headlong/abi/DecodeTest.java
+++ src/test/java/com/esaulpaugh/headlong/abi/DecodeTest.java
@@ -915,7 +915,7 @@ public class DecodeTest {
 
     @Test
     public void testLegacyDecode() throws Throwable {
-        final Function f = Function.fromJson(FN_JSON, ABIType.FLAG_LEGACY_DECODE);
+        final Function f = Function.fromJson(ABIType.FLAG_LEGACY_DECODE, FN_JSON);
         final Function f2 = (Function) ABIJSON.parseElements(ABIType.FLAG_LEGACY_DECODE, "[" + FN_JSON + "]", ABIJSON.ALL).get(0);
         checkLegacyFlags(f.getInputs());
         checkLegacyFlags(f2.getInputs());
