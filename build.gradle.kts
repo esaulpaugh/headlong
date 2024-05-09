54ddb4010094e83a386478290da134565b1c0630
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2024-05-08 20:25:13 -0500

    update commons-codec version

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,7 +85,7 @@
 //    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 //    testImplementation("org.bouncycastle:bcprov-jdk14:$bcVersion")
 //
-//    jmhImplementation("commons-codec:commons-codec:1.16.1")
+//    jmhImplementation("commons-codec:commons-codec:1.17.0")
 //}
 //
 //jmh {
