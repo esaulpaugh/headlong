f90411e260263ce1d332ed1d3d35d8fbaaabeec2
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2024-06-15 03:00:15 -0500

    remove unused import

--- src/test/java/com/esaulpaugh/headlong/abi/EncodeTest.java
+++ src/test/java/com/esaulpaugh/headlong/abi/EncodeTest.java
@@ -53,7 +53,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EncodeTest {
