f8237f9acdc5f6387dfe98d4199abef87f58324e
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2025-03-18 18:20:23 -0500

    refactor initInstances

--- src/main/java/com/esaulpaugh/headlong/abi/UnitType.java
+++ src/main/java/com/esaulpaugh/headlong/abi/UnitType.java
@@ -21,6 +21,7 @@ import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static com.esaulpaugh.headlong.abi.ArrayType.DYNAMIC_LENGTH;
@@ -217,72 +218,69 @@ public abstract class UnitType<J> extends ABIType<J> { // J generally extends Nu
         return LEGACY_BASE_TYPE_MAP.get(rawType);
     }
 
-    static synchronized void initInstances() {
-        // synchronized won't prevent method reentry when we trigger the initialization of a subclass of UnitType, which will call initInstances again
-        // so add at least one non-UnitType entry to the map before touching any of UnitType's subclasses!
-        if (!BASE_TYPE_MAP.isEmpty()) {
-            return;
-        }
-        final long initialCount = INSTANCE_COUNT.get();
-        if (initialCount < 0L || initialCount > 2L) { // will be the number of static instances in whichever subclass is initialized first
-            throw new AssertionError(initialCount + " instances");
-        }
-        if (!LEGACY_BASE_TYPE_MAP.isEmpty()) {
-            throw new AssertionError("map not empty");
-        }
+    private static final AtomicBoolean MAP_INITIALIZED = new AtomicBoolean(false);
 
-        final Map<String, ABIType<?>> map = BASE_TYPE_MAP;
-
-        // first entry is not a UnitType, because of reentrancy. see above.
-        map.put("string", new ArrayType<ByteType, Byte, String>("string", STRING_CLASS, ByteType.INSTANCE, DYNAMIC_LENGTH, STRING_ARRAY_CLASS, ABIType.FLAGS_NONE));
-
-        // optimized insertion order
-        map.put("bool", BooleanType.INSTANCE);
-
-        for (int n = 1; n <= 32; n++) {
-            mapByteArray(map, "bytes" + n, n);
-        }
-        mapByteArray(map, "function", FUNCTION_BYTE_LEN);
-        mapByteArray(map, "bytes", DYNAMIC_LENGTH);
-
-        for (int n = 8; n <= 24; n += 8) mapInt(map, "uint" + n, n, true); // will trigger IntType initialization, which will init UINT21 and UINT31
-        for (int n = 32; n <= 56; n += 8) mapLong(map, "uint" + n, n, true);
-        for (int n = 64; n <= 256; n += 8) mapBigInteger(map, "uint" + n, n, true);
-
-        map.put("uint", map.get("uint256"));
-
-        mapBigInteger(map, "int256", 256, false);
-        map.put("int", map.get("int256"));
-
-        for (int n = 8; n <= 32; n += 8) mapInt(map, "int" + n, n, false);
-        for (int n = 40; n <= 64; n += 8) mapLong(map, "int" + n, n, false);
-        for (int n = 72; n < 256; n += 8) mapBigInteger(map, "int" + n, n, false);
-
-        map.put("address", AddressType.INSTANCE);
-
-        map.put("fixed128x18", new BigDecimalType("fixed128x18", FIXED_BIT_LEN, FIXED_SCALE, false));
-        map.put("ufixed128x18", new BigDecimalType("ufixed128x18", FIXED_BIT_LEN, FIXED_SCALE, true));
-
-        map.put("decimal", map.get("int168"));
-        map.put("fixed", map.get("fixed128x18"));
-        map.put("ufixed", map.get("ufixed128x18"));
-
-        for (Map.Entry<String, ABIType<?>> e : map.entrySet()) {
-            ABIType<?> value = e.getValue();
-            if (value instanceof ArrayType) {
-                final ArrayType<?, ?, ?> at = value.asArrayType();
-                value = new ArrayType<>(at.canonicalType, at.clazz, ByteType.INSTANCE, at.getLength(), at.arrayClass(), ABIType.FLAG_LEGACY_DECODE);
+    static void initInstances() {
+        if (MAP_INITIALIZED.compareAndSet(false, true)) {
+            if (!BASE_TYPE_MAP.isEmpty() || !LEGACY_BASE_TYPE_MAP.isEmpty()) {
+                throw new AssertionError("map not empty");
             }
-            LEGACY_BASE_TYPE_MAP.put(e.getKey(), value);
-        }
+            final long initialCount = INSTANCE_COUNT.get();
+            if (initialCount < 0L || initialCount > 2L) { // will be the number of static instances in whichever subclass is initialized first
+                throw new AssertionError(initialCount + " instances");
+            }
 
-        final int full = 108;
-        if (BASE_TYPE_MAP.size() != full || LEGACY_BASE_TYPE_MAP.size() != full) {
-            throw new AssertionError("map not full");
-        }
-        final long finalCount = INSTANCE_COUNT.get();
-        if (finalCount != INSTANCE_LIMIT) {
-            throw new AssertionError(finalCount + " instances");
+            final Map<String, ABIType<?>> map = BASE_TYPE_MAP;
+
+            // optimized insertion order
+            map.put("bool", BooleanType.INSTANCE);
+            map.put("string", new ArrayType<ByteType, Byte, String>("string", STRING_CLASS, ByteType.INSTANCE, DYNAMIC_LENGTH, STRING_ARRAY_CLASS, ABIType.FLAGS_NONE));
+
+            for (int n = 1; n <= 32; n++) {
+                mapByteArray(map, "bytes" + n, n);
+            }
+            mapByteArray(map, "function", FUNCTION_BYTE_LEN);
+            mapByteArray(map, "bytes", DYNAMIC_LENGTH);
+
+            for (int n = 8; n <= 24; n += 8) mapInt(map, "uint" + n, n, true); // will trigger IntType initialization, which will init UINT21 and UINT31
+            for (int n = 32; n <= 56; n += 8) mapLong(map, "uint" + n, n, true);
+            for (int n = 64; n <= 256; n += 8) mapBigInteger(map, "uint" + n, n, true);
+
+            map.put("uint", map.get("uint256"));
+
+            mapBigInteger(map, "int256", 256, false);
+            map.put("int", map.get("int256"));
+
+            for (int n = 8; n <= 32; n += 8) mapInt(map, "int" + n, n, false);
+            for (int n = 40; n <= 64; n += 8) mapLong(map, "int" + n, n, false);
+            for (int n = 72; n < 256; n += 8) mapBigInteger(map, "int" + n, n, false);
+
+            map.put("address", AddressType.INSTANCE);
+
+            map.put("fixed128x18", new BigDecimalType("fixed128x18", FIXED_BIT_LEN, FIXED_SCALE, false));
+            map.put("ufixed128x18", new BigDecimalType("ufixed128x18", FIXED_BIT_LEN, FIXED_SCALE, true));
+
+            map.put("decimal", map.get("int168"));
+            map.put("fixed", map.get("fixed128x18"));
+            map.put("ufixed", map.get("ufixed128x18"));
+
+            for (Map.Entry<String, ABIType<?>> e : map.entrySet()) {
+                ABIType<?> value = e.getValue();
+                if (value instanceof ArrayType) {
+                    final ArrayType<?, ?, ?> at = value.asArrayType();
+                    value = new ArrayType<>(at.canonicalType, at.clazz, ByteType.INSTANCE, at.getLength(), at.arrayClass(), ABIType.FLAG_LEGACY_DECODE);
+                }
+                LEGACY_BASE_TYPE_MAP.put(e.getKey(), value);
+            }
+
+            final int full = 108;
+            if (BASE_TYPE_MAP.size() != full || LEGACY_BASE_TYPE_MAP.size() != full) {
+                throw new AssertionError("map not full");
+            }
+            final long finalCount = INSTANCE_COUNT.get();
+            if (finalCount != INSTANCE_LIMIT) {
+                throw new AssertionError(finalCount + " instances");
+            }
         }
     }
 
