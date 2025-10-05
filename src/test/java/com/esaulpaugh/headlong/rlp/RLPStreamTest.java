b6d51f388cee2425ebb88e5ff5cb19fb45e0b436
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2025-10-05 01:22:39 -0500

    improve test

--- src/test/java/com/esaulpaugh/headlong/rlp/RLPStreamTest.java
+++ src/test/java/com/esaulpaugh/headlong/rlp/RLPStreamTest.java
@@ -71,8 +71,8 @@ public class RLPStreamTest {
     public void testStreamRoundTrip() throws IOException {
         final byte[] mysteryBytes = new byte[5];
         TestUtils.seededRandom().nextBytes(mysteryBytes);
-        final byte[][] complexData = {
-                {},
+        final byte[][] sequence = {
+                Strings.EMPTY_BYTE_ARRAY,
                 TEST_STRING.getBytes(StandardCharsets.UTF_8),
                 mysteryBytes
         };
@@ -81,13 +81,14 @@ public class RLPStreamTest {
         try (RLPOutputStream ros = new RLPOutputStream(baos)) {
             ros.write(Integer.MAX_VALUE);
             ros.write(Integer.MIN_VALUE);
-            for (byte[] e : complexData) ros.write(e);
-            for (byte[] e : complexData) ros.write(e, 0, e.length);
+            ros.writeSequence((Object[])sequence);
+            for (byte[] e : sequence) ros.write(e);
+            for (byte[] e : sequence) ros.write(e, 0, e.length);
             ros.write(127);
             ros.write(0);
             ros.writeList(Collections.singletonList(new byte[]{-1}));
             ros.write(Short.MAX_VALUE);
-            ros.writeSequence(new byte[] { 1, 0 }, new Object[] { new byte[] { 1, 1 } }, new byte[0]);
+            ros.writeSequence(new byte[] { 1, 0 }, new Object[] { new byte[] { 1, 1 } }, Strings.EMPTY_BYTE_ARRAY);
             ros.write(Short.MIN_VALUE);
         }
 
@@ -102,29 +103,28 @@ public class RLPStreamTest {
         assertArrayEquals(Integers.toBytes(Integer.MAX_VALUE), iter.next().asBytes());
         assertArrayEquals(Integers.toBytes(Integer.MIN_VALUE), iter.next().asBytes());
 
-        assertArrayEquals(new byte[] {}, iter.next().asBytes());
-        assertEquals(TEST_STRING, new String(iter.next().asBytes(), StandardCharsets.UTF_8));
-        assertArrayEquals(mysteryBytes, iter.next().asBytes());
-
-        assertArrayEquals(new byte[] {}, iter.next().asBytes());
-        assertEquals(TEST_STRING, new String(iter.next().asBytes(), StandardCharsets.UTF_8));
-        assertArrayEquals(mysteryBytes, iter.next().asBytes());
+        for (int i = 0; i < 3; i++) {
+            assertArrayEquals(Strings.EMPTY_BYTE_ARRAY, iter.next().asBytes());
+            assertEquals(TEST_STRING, new String(iter.next().asBytes(), StandardCharsets.UTF_8));
+            assertArrayEquals(mysteryBytes, iter.next().asBytes());
+        }
 
         assertArrayEquals(new byte[] { 0x7f }, iter.next().asBytes());
         assertArrayEquals(new byte[] { 0x00 }, iter.next().asBytes());
 
         RLPList list = iter.next().asRLPList();
         assertEquals("c281ff", list.encodingString(Strings.HEX));
-        assertEquals(1, list.elements().size());
-        assertEquals(-1, list.elements().get(0).asByte());
+        List<RLPItem> elements = list.elements();
+        assertEquals(1, elements.size());
+        assertEquals(-1, elements.get(0).asByte());
 
         assertArrayEquals(Integers.toBytes(Short.MAX_VALUE), iter.next().asBytes());
 
         assertArrayEquals(new byte[] {1,0}, iter.next().asBytes());
-        RLPList nested = iter.next().asRLPList();
-        assertEquals(1, nested.elements().size());
-        assertArrayEquals(new byte[] {1,1}, nested.elements().get(0).asBytes());
-        assertArrayEquals(new byte[0], iter.next().asBytes());
+        elements = iter.next().asRLPList().elements();
+        assertEquals(1, elements.size());
+        assertArrayEquals(new byte[] {1,1}, elements.get(0).asBytes());
+        assertArrayEquals(Strings.EMPTY_BYTE_ARRAY, iter.next().asBytes());
 
         assertEquals("ffff8000", Strings.encode(iter.next().asBytes()));
         assertFalse(iter.hasNext());
