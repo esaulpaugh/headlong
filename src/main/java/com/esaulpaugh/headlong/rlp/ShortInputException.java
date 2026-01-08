68e2d825a632a0b08685029833db6feb5329f22f
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2026-01-08 15:06:57 -0600

    update some copyright notices

--- src/main/java/com/esaulpaugh/headlong/rlp/ShortInputException.java
+++ src/main/java/com/esaulpaugh/headlong/rlp/ShortInputException.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2019 Evan Saulpaugh
+   Copyright 2019-2026 Evan Saulpaugh
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
