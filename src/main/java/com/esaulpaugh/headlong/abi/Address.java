4bc9f9fd4521356d30b69011f1c44b6b3bcd0489
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2026-01-08 15:11:04 -0600

    update some copyright notices

--- src/main/java/com/esaulpaugh/headlong/abi/Address.java
+++ src/main/java/com/esaulpaugh/headlong/abi/Address.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2021 Evan Saulpaugh
+   Copyright 2021-2026 Evan Saulpaugh
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
