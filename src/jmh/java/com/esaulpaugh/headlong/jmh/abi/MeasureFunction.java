3a40b404efad5433117aebe692514dc05fa31086
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2026-01-08 15:15:03 -0600

    update some copyright notices

--- src/jmh/java/com/esaulpaugh/headlong/jmh/abi/MeasureFunction.java
+++ src/jmh/java/com/esaulpaugh/headlong/jmh/abi/MeasureFunction.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2018 Evan Saulpaugh
+   Copyright 2019-2026 Evan Saulpaugh
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
