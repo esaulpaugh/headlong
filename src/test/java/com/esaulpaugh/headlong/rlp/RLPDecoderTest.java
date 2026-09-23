9e41db1995785ebd4e7eaa40e93d030ca3eba150
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2026-09-23 01:18:49 -0600

    unused import

--- src/test/java/com/esaulpaugh/headlong/rlp/RLPDecoderTest.java
+++ src/test/java/com/esaulpaugh/headlong/rlp/RLPDecoderTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.math.BigInteger;
 import java.nio.BufferOverflowException;
