4b03590d54f02fc2436d0602a790bed4b6b20c8f
Evan Saulpaugh <evan.saulpaugh@gmail.com>
2026-09-22 21:11:32 -0600

    optimize TestUtils method

--- src/test/java/com/esaulpaugh/headlong/TestUtils.java
+++ src/test/java/com/esaulpaugh/headlong/TestUtils.java
@@ -180,7 +180,7 @@ public final class TestUtils {
         long sample;
         do {
             sample = r.nextLong();
-            if (sample < 0L) sample = ~sample;
+            sample ^= (sample >> 63);
         } while (sample >= bound);
         return sample % limit;
     }
